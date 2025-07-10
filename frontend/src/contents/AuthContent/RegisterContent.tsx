e4248ef4e0ed0f7c4cea26fe21a077e5070a9dc4
Liv44 <moreauolivia49@gmail.com>
2025-07-10 17:50:45 +0200

    feat(Connexion): add tanstack mutations to login and signin

--- frontend/src/contents/AuthContent/RegisterContent.tsx
+++ frontend/src/contents/AuthContent/RegisterContent.tsx
@@ -3,12 +3,13 @@ import { useTranslation } from 'react-i18next';
 import z from 'zod';
 
 import AuthForm, { FieldConfig } from '@/components/Auth/AuthForm';
-import { supabase } from '@/config/supabaseConfig';
+import useSignIn from '@/domain/usecases/auth/useSignIn';
 
 const RegisterContent = () => {
   const [emailSent, setEmailSent] = useState(false);
   const [errorSendingEmail, setErrorSendingEmail] = useState(false);
   const { t } = useTranslation();
+  const { mutate: signIn } = useSignIn();
 
   const registerFormSchema = z.object({
     pseudo: z
@@ -38,23 +39,19 @@ const RegisterContent = () => {
   const onSubmitRegister = async (
     values: z.infer<typeof registerFormSchema>
   ) => {
-    console.log(values);
     setErrorSendingEmail(false);
-    const { error } = await supabase.auth.signInWithOtp({
-      email: values.email,
-      options: {
-        emailRedirectTo: `${window.location.origin}/`,
-        data: {
-          pseudo: values.pseudo,
+    setEmailSent(false);
+    signIn(
+      { email: values.email, pseudo: values.pseudo },
+      {
+        onSuccess: () => {
+          setEmailSent(true);
         },
-      },
-    });
-    if (error) {
-      setErrorSendingEmail(true);
-      console.error(error);
-    } else {
-      setEmailSent(true);
-    }
+        onError: () => {
+          setErrorSendingEmail(true);
+        },
+      }
+    );
   };
   return (
     <AuthForm
---
+++ frontend/src/domain/usecases/auth/useLogin.ts
@@ -0,0 +1,22 @@
+import { supabase } from '@/config/supabaseConfig';
+import { AuthError } from '@supabase/supabase-js';
+import { useMutation } from '@tanstack/react-query';
+
+const useLogin = () => {
+  return useMutation<void, AuthError, { email: string }>({
+    mutationFn: async ({ email }) => {
+      const { error } = await supabase.auth.signInWithOtp({
+        email,
+        options: {
+          shouldCreateUser: false,
+          emailRedirectTo: `${window.location.origin}/`,
+        },
+      });
+      if (error) {
+        throw error;
+      }
+    },
+  });
+};
+
+export default useLogin;
