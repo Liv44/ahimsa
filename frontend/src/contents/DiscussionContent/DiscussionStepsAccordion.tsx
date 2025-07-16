cfce5039b17288b375e245bf45e451bbea6981b2
Liv44 <moreauolivia49@gmail.com>
2025-07-16 19:40:26 +0200

    feat(Discussion): add error message when a step is not completed

--- frontend/src/contents/DiscussionContent/DiscussionStepsAccordion.tsx
+++ frontend/src/contents/DiscussionContent/DiscussionStepsAccordion.tsx
@@ -30,12 +30,18 @@ const DiscussionStepsAccordion = () => {
   const [content, setContent] = useState<string[]>(
     steps.map((step) => step.content)
   );
+  const [error, setError] = useState<boolean>(false);
   const [activeStep, setActiveStep] = useState<number>(0);
 
   const navigate = useNavigate();
 
   const handleNextStep = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setError(false);
+    if (content[activeStep] === '') {
+      setError(true);
+      return;
+    }
     setActiveStep(activeStep + 1);
     steps[activeStep].updateContent(content[activeStep]);
     steps[activeStep].complete();
@@ -95,7 +101,9 @@ const DiscussionStepsAccordion = () => {
                 {t(`discussion-page.step.${steps[index].key}.label`)}
               </Label>
               <Textarea
-                className="bg-white text-black text-sm"
+                className={`bg-white text-black text-sm ${
+                  error ? 'border-red-500 border-2' : ''
+                }`}
                 name={steps[index].key}
                 id={steps[index].key}
                 placeholder={t(
@@ -113,6 +121,11 @@ const DiscussionStepsAccordion = () => {
                   })
                 }
               />
+              {error && (
+                <p className="text-red-500 text-sm text-center bg-white rounded-lg p-2 mx-auto">
+                  {t('discussion-page.step.error')}
+                </p>
+              )}
               <Button
                 type="submit"
                 className="bg-orange text-black hover:bg-orange/80 max-w-40 mx-auto"
