8b2fc0d1ff789f8c6f7a8e54a8f5fb7cad7425c1
Liv44 <moreauolivia49@gmail.com>
2025-07-18 13:55:18 +0200

    refactor(frontend): move all files from frontend folder to main folder

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,16 @@
-// commitlint.config.js
-module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'chore', 'refactor', 'test', 'style']],
-        'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case']],
-        'scope-empty': [2, 'never']
-    }
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'chore', 'refactor', 'test', 'style'],
+    ],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case'],
+    ],
+    'scope-empty': [2, 'never'],
+  },
 };
